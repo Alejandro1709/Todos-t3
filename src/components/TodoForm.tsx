75fa88cc4bba374d36955806df49e8e567a7802d
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-09 10:53:47 -0500

    Add: useRef hook to input for controlling the input itself

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,10 +1,32 @@
+import { useRef } from "react";
+
 function TodoForm() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!inputRef.current) return;
+
+    inputRef.current.value = e.target.value;
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (inputRef.current) {
+      console.log(inputRef.current.value);
+    }
+  };
+
   return (
-    <form className="flex w-full flex-row items-center justify-between overflow-hidden rounded-md bg-gray-700 shadow">
+    <form
+      className="flex w-full flex-row items-center justify-between overflow-hidden rounded-md bg-gray-700 shadow"
+      onSubmit={handleSubmit}
+    >
       <input
         className="w-full bg-transparent p-3 text-white outline-none"
         type="text"
         placeholder="Buy Milk.."
+        onChange={handleChange}
+        ref={inputRef}
       />
       <button
         className="self-stretch bg-gray-600 px-3 text-white hover:bg-gray-500"
